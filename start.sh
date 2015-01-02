04c985896890a6167e972676b36597b78b5e0654
Iain Mckay <iainmckay@ecenglish.com>
2015-01-02 10:08:42 +0100

    Updated paths in start.sh to work with git version

--- start.sh
+++ start.sh
@@ -1,18 +1,18 @@
 #! /bin/sh
 
-cd /sickbeard
+cd /sickbeard/sickbeard
 if [ -f /config/config.ini ]
 then
-	rm -rf /sickbeard/config.ini
-	rm -rf /sickbeard/sickbeard.db
+	rm -rf /sickbeard/sickbeard/config.ini
+	rm -rf /sickbeard/sickbeard/sickbeard.db
 #	rm -rf /sickbeard/autoprocesstv/autoProcessTV.cfg
 else
-	mv -f /sickbeard/config.ini /config/config.ini
-	mv -f /sickbeard/sickbeard.db /config/sickbeard.db
+	mv -f /sickbeard/sickbeard/config.ini /config/config.ini
+	mv -f /sickbeard/sickbeard/sickbeard.db /config/sickbeard.db
 #	mv -f /sickbeard/autoprocesstv/autoProcessTV.cfg /config/autoProcessTV.cfg
 fi
-ln -sf /config/config.ini /sickbeard/
-ln -sf /config/sickbeard.db /sickbeard/sickbeard.db
+ln -sf /config/config.ini /sickbeard/sickbeard/
+ln -sf /config/sickbeard.db /sickbeard/sickbeard/sickbeard.db
 #ln -sf /config/autoProcessTV.cfg sickbeard/autoprocesstv/autoProcessTV.cfg
 
 /usr/bin/python SickBeard.py
